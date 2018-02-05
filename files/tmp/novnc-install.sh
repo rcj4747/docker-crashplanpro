f7f083c0c52b0dcdfee77f8da37f91bec7130224
Robert C Jennings <robert.jennings@canonical.com>
2018-02-05 11:18:05 -0600

    Switch to versioned downloads for github-based code

--- files/tmp/novnc-install.sh
+++ files/tmp/novnc-install.sh
@@ -2,7 +2,9 @@
 
 # Install noVNC
 mkdir /opt/novnc
-curl -L https://github.com/kanaka/noVNC/archive/master.tar.gz | tar -xz --strip=1 -C /opt/novnc
+curl -L https://github.com/novnc/noVNC/archive/stable/v0.6.tar.gz | \
+    tar --extract --gzip --strip=1 --directory=/opt/novnc
 
 mkdir -p /opt/novnc/utils/websockify
-curl -L https://github.com/kanaka/websockify/archive/master.tar.gz | tar -xz --strip=1 -C /opt/novnc/utils/websockify
+curl -L https://github.com/novnc/websockify/archive/v0.8.0.tar.gz | \
+    tar --extract --gzip --strip=1 --directory=/opt/novnc/utils/websockify
--- files/tmp/tigervnc-install.sh
+++ files/tmp/tigervnc-install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Install TigerVNC
-curl -L https://dl.bintray.com/tigervnc/stable/tigervnc-1.7.0.x86_64.tar.gz | tar -xz --strip=1 -C /
+# Install TigerVNC (Drop on move to Ubuntu Artful of later for archive version)
+curl -L https://dl.bintray.com/tigervnc/stable/tigervnc-1.8.0.x86_64.tar.gz | \
+    tar --extract --gzip --strip=1 --directory=/
 
 # vncpasswd.py
 mkdir -p /opt/vncpasswd
-curl -L https://github.com/trinitronx/vncpasswd.py/archive/master.tar.gz | tar -zx --strip=1 -C /opt/vncpasswd
+curl -L https://github.com/trinitronx/vncpasswd.py/archive/v1.2.0.tar.gz | \
+    tar --extract --gzip --strip=1 --directory=/opt/vncpasswd
